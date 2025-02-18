2b66005de241f9d0450613fe6840212f7aef8f77
thavixt <komlosi.peti@gmail.com>
2025-02-18 18:58:04 +0100

    feat: remove prettier

--- eslint.config.js
+++ eslint.config.js
@@ -4,7 +4,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
-	{ ignores: ["lib", "dist"] },
+	{ ignores: ["dist"] },
 	{
 		extends: [js.configs.recommended, ...tseslint.configs.recommended],
 		files: ["**/*.{ts,tsx}"],
